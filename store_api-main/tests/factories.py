55a067c0f88266a04956f9e710a872aade7c848f
BiSousa <151962990+BiSousa@users.noreply.github.com>
2024-07-11 15:34:51 -0300

    Add files

---
+++ store_api-main/tests/factories.py
@@ -0,0 +1,21 @@
+def product_data():
+    return {
+        "name": "Iphone 14 Pro Max",
+        "quantity": 8,
+        "price": "8.500",
+        "status": True,
+    }
+
+
+def products_data():
+    return [
+        {"name": "Iphone 11 Pro Max", "quantity": 15, "price": "3.500", "status": True},
+        {"name": "Iphone 12 Pro Max", "quantity": 10, "price": "4.500", "status": True},
+        {"name": "Iphone 13 Pro Max", "quantity": 2, "price": "5.500", "status": True},
+        {
+            "name": "Iphone 15 Pro Max",
+            "quantity": 2,
+            "price": "9.500",
+            "status": False,
+        },
+    ]
